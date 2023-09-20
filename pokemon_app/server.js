4085489adf61e5a088b068d7ecc59826b3c7b1cd
Fan Yang <yangfanhewangzheng@gmail.com>
2023-09-20 15:23:57 -0400

    w12d2hw

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -1,26 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3001
-const pokemon = require('./models/pokemon')
-const jsxEngine = require('jsx-view-engine')
+const express = require("express");
+const app = express();
+const port = 3001;
+const pokemon = require("./models/pokemon");
+const jsxEngine = require("jsx-view-engine");
 
-app.set('view engine', 'jsx')
-app.engine('jsx', jsxEngine())
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine());
 
-app.get('/', (req,res)=>{
-    res.send('Welcome to the Pokemon App!')
+app.use(express.urlencoded({extended:false}))
+app.use((req, res, next)=>{
+    console.log('I run for all routes')
+    next()
 })
 
-app.get('/pokemon', (req, res)=>{
-    // res.send(pokemon)
-    res.render('Index', {pokemon: pokemon})
+app.get("/", (req, res) => {
+  res.send("Welcome to the Pokemon App!");
+});
+
+app.get("/pokemon", (req, res) => {
+  // res.send(pokemon)
+  res.render("Index", { pokemon: pokemon });
+});
+
+app.get("/pokemon/new", (req, res) => {
+  res.render("New");
+});
+
+app.post('/pokemon', (req, res)=>{
+    pokemon.push(req.body)
+    console.log(pokemon)
+    res.redirect('/pokemon')
 })
 
-app.get('/pokemon/:id', (req, res)=>{
-    res.render('Show', {pokemon: pokemon[req.params.id]})
-})
+app.get("/pokemon/:id", (req, res) => {
+  res.render("Show", { pokemon: pokemon[req.params.id] });
+});
 
-
-app.listen(3001,()=>{
-    console.log("listening")
-})
+app.listen(3001, () => {
+  console.log("listening");
+});
--- pokemon_app/views/Index.jsx
+++ pokemon_app/views/Index.jsx
@@ -1,27 +1,31 @@
-const React = require('react')
+const React = require("react");
 const myStyle = {
-    color: "#ffffff",
-    backgroundColor: "#000000",
-  };
-class Index extends React. Component{
-    render(){
-        const {pokemon} = this.props
-        return(
-            <div>
-                <h1 style={myStyle}>See All The Pokemon!</h1>
-                <ul>
-                   {pokemon.map((pokemon, i)=>{
-                    return(
-                        <li>
-                           <a href={`/pokemon/${i}`}>{pokemon.name.charAt(0).toUpperCase()+ pokemon.name.slice(1).toLowerCase()}</a> 
-                        </li>
-                    )
-
-                   })} 
-                </ul>
-            </div>
-
-        )
-    }
+  color: "#ffffff",
+  backgroundColor: "#000000",
+};
+class Index extends React.Component {
+  render() {
+    const { pokemon } = this.props;
+    return (
+      <div>
+        <h1 style={myStyle}>See All The Pokemon!</h1>
+        <ul>
+          {pokemon.map((pokemon, i) => {
+            return (
+              <li>
+                <a href={`/pokemon/${i}`}>
+                  {pokemon.name.charAt(0).toUpperCase() +
+                    pokemon.name.slice(1).toLowerCase()}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <nav>
+          <a href="/pokemon/new">Creat a New Pokemon</a>
+        </nav>
+      </div>
+    );
+  }
 }
-module.exports = Index
+module.exports = Index;
